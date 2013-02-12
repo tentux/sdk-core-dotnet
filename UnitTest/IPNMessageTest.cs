9273aa5e914552b67478294e4f892d2381072c50
lathavairamani <lvairamani@paypal.com>
2013-02-12 11:09:09 +0530

    updated IPN test case for IPNMessage fix

--- UnitTest/IPNMessageTest.cs
+++ UnitTest/IPNMessageTest.cs
@@ -11,22 +11,23 @@ namespace PayPal.UnitTest
     [TestFixture]
     class IPNMessageTest
     {
-        string ipnMsg = "fees_payer=EACHRECEIVER&payment_request_date=Thu+Dec+06+22%3A50%3A00+PST+2012&transaction[0].is_primary_receiver=false&transaction[0].pending_reason=NONE&cancel_url=http%3A%2F%2Flocalhost%3A9080%2Fadaptivepayments-sample%2Findex.html&status=COMPLETED&transaction_type=Adaptive+Payment+PAY&transaction[0].status=Completed&verify_sign=AM1sBeDL1IjnsgstrDz8f0QWZStzApiXR3gXXjJUE15uzMlXzQmgS-.C&charset=windows-1252&sender_email=jb-us-seller%40paypal.com&log_default_shipping_address_in_transaction=false&transaction[0].amount=USD+2.00&pay_key=AP-70354820B64901803&reverse_all_parallel_payments_on_error=false&ipn_notification_url=https%3A%2F%2Fnpi.pagekite.me%2Fadaptivepaymentssample%2FIPNListener&transaction[0].id=0UJ53158NW5107715&return_url=http%3A%2F%2Flocalhost%3A9080%2Fadaptivepayments-sample%2Findex.html&transaction[0].receiver=platfo_1255612361_per%40gmail.com&transaction[0].id_for_sender_txn=8UL93971B69293341&action_type=PAY&notify_version=UNVERSIONED&transaction[0].status_for_sender_txn=Completed&test_ipn=1";
+        private string ipnMsg = "fees_payer=EACHRECEIVER&payment_request_date=Thu+Dec+06+22%3A50%3A00+PST+2012&transaction[0].is_primary_receiver=false&transaction[0].pending_reason=NONE&cancel_url=http%3A%2F%2Flocalhost%3A9080%2Fadaptivepayments-sample%2Findex.html&status=COMPLETED&transaction_type=Adaptive+Payment+PAY&transaction[0].status=Completed&verify_sign=AM1sBeDL1IjnsgstrDz8f0QWZStzApiXR3gXXjJUE15uzMlXzQmgS-.C&charset=windows-1252&sender_email=jb-us-seller%40paypal.com&log_default_shipping_address_in_transaction=false&transaction[0].amount=USD+2.00&pay_key=AP-70354820B64901803&reverse_all_parallel_payments_on_error=false&ipn_notification_url=https%3A%2F%2Fnpi.pagekite.me%2Fadaptivepaymentssample%2FIPNListener&transaction[0].id=0UJ53158NW5107715&return_url=http%3A%2F%2Flocalhost%3A9080%2Fadaptivepayments-sample%2Findex.html&transaction[0].receiver=platfo_1255612361_per%40gmail.com&transaction[0].id_for_sender_txn=8UL93971B69293341&action_type=PAY&notify_version=UNVERSIONED&transaction[0].status_for_sender_txn=Completed&test_ipn=1";
         NameValueCollection ipnMap = new NameValueCollection();
-
+        /// <summary>
+        /// Encoding format for IPN messages
+        /// </summary>
+        private const string IPNEncoding = "windows-1252";
         [Test]
         public void IPNRequest()
         {
-            NameValueCollection nvc = HttpUtility.ParseQueryString(ipnMsg);
-            IPNMessage ipn = new IPNMessage(nvc); 
+            IPNMessage ipn = new IPNMessage(Encoding.GetEncoding(IPNEncoding).GetBytes(ipnMsg)); 
             Assert.IsTrue(ipn.Validate());
         }
 
         [Test]
 	    public void IPNMap() 
         {
-            NameValueCollection nvc = HttpUtility.ParseQueryString(ipnMsg);
-            IPNMessage ipn = new IPNMessage(nvc);
+            IPNMessage ipn = new IPNMessage(Encoding.GetEncoding(IPNEncoding).GetBytes(ipnMsg)); 
             NameValueCollection ipnMap = ipn.IpnMap;
             Assert.IsNotNull(ipnMap);
 	    }
@@ -34,8 +35,7 @@ namespace PayPal.UnitTest
         [Test]
         public void IPNTransaction()
         {
-            NameValueCollection nvc = HttpUtility.ParseQueryString(ipnMsg);
-            IPNMessage ipn = new IPNMessage(nvc); 
+            IPNMessage ipn = new IPNMessage(Encoding.GetEncoding(IPNEncoding).GetBytes(ipnMsg)); 
             string transactionType = ipn.TransactionType;
             Assert.AreEqual("Adaptive Payment PAY", transactionType);
         }
@@ -43,8 +43,7 @@ namespace PayPal.UnitTest
         [Test]
         public void IPNParameter()
         {
-            NameValueCollection nvc = HttpUtility.ParseQueryString(ipnMsg);
-            IPNMessage ipn = new IPNMessage(nvc); 
+            IPNMessage ipn = new IPNMessage(Encoding.GetEncoding(IPNEncoding).GetBytes(ipnMsg)); 
             string parameter = ipn.IpnValue("fees_payer");
             Assert.AreEqual("EACHRECEIVER", parameter);
         }        
@@ -52,7 +51,7 @@ namespace PayPal.UnitTest
         [Test]
         public void IPNParseQueryString()
         {
-            NameValueCollection nvc = HttpUtility.ParseQueryString(ipnMsg);
+            NameValueCollection nvc = HttpUtility.ParseQueryString(ipnMsg,Encoding.GetEncoding(IPNEncoding));
             if (nvc.HasKeys())
             {
                 ipnMap.Add(nvc.GetKey(0), nvc.Get(0));
